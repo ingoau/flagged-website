a58104620a1fd28a3f1f010220cd34b76f2dac51
Ingo Wolf <me@ingowolf.au>
2026-03-11 22:26:34 +1100

    add font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,28 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.local(),
+        name: "GeistMonoNerdFont",
+        cssVariable: "--geist-mono-nerd-font",
+        options: {
+          variants: [
+            {
+              src: ["./src/assets/fonts/GeistMonoNerdFontMono-Regular.otf"],
+              weight: "normal",
+              style: "normal",
+            },
+          ],
+        },
+      },
+    ],
+  },
   vite: {
     plugins: [tailwindcss()],
   },
